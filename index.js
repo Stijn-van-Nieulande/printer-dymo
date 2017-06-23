ec545b6af8ea4be76ac2f3dab3c15efd7c060d04
Paul Prins <prinsp@freshvine.co>
2017-06-23 22:06:13 +0200

    Updated getPrinter functions
    Lets you do this async, sycn, or to get a specific printer

--- index.js
+++ index.js
@@ -40,26 +40,55 @@ var ready = module.exports.ready = function(){
 }
 
 
+/** Get a list of all the printers
+ * @param printerName printer name to extract the info
+ * @return printer object info:
+ */
+ var getPrinters = module.exports.getPrinters = function( callback ){
+	var availablePrinters = edge.func({
+		assemblyFile: nodeDymoLib,
+		typeName: 'NodeDymoLib.Dymo',
+		methodName: 'Printers'
+	});
+	availablePrinters('', callback );
+	return;
+}
+
+/**
+ *	Same as getPrinters but syncronice
+ */
+ var getPrintersSync = module.exports.getPrintersSync = function(){
+	if( initReady != true ){	}
+	
+	var availablePrinters = edge.func({
 		assemblyFile: nodeDymoLib,
 		typeName: 'NodeDymoLib.Dymo',
 		methodName: 'Printers'
 	});
 
-	var print = module.exports.print = edge.func({
-		assemblyFile: nodeDymoLib,
-		typeName: 'NodeDymoLib.Dymo',
-		methodName: 'Print'
-	});
+	return availablePrinters( '', true );
 }
 
-for (var f of dymoAssemblies) {
-	var source = path.join(dymoLibPath, f), target = path.join(libDir, f);
-	var readStream = fs.createReadStream(source);
-	var writeStream = fs.createWriteStream(target);
-	readStream.on('error', function(err) { throw err });
-	writeStream.on('error', function(err) { throw err });
-	writeStream.on('finish', initDymoLib);
-	readStream.pipe(writeStream);
+
+var getPrinter = module.exports.getPrinterSync = function( thisPrinterName ){
+	if( thisPrinterName == '' ){ return true; }
+	
+	var availablePrinters = edge.func({
+		assemblyFile: nodeDymoLib,
+		typeName: 'NodeDymoLib.Dymo',
+		methodName: 'Printers'
+	});
+
+	var tempPrinters = availablePrinters( '', true );
+	for( var i in tempPrinters ){
+		console.log( tempPrinters[i] );
+		if( tempPrinters[i].Name == thisPrinterName ){
+			return tempPrinters[i];
+		}
+	}
+
+	return false;
+}
 }
 
 
