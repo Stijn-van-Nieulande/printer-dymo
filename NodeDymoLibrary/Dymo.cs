4150c8706a41e43e665445e768a2b73a253a864a
Paul Prins <prinsp@freshvine.co>
2017-06-23 21:29:54 +0200

    Update Print function to use jobs and multiple labels

--- NodeDymoLibrary/Dymo.cs
+++ NodeDymoLibrary/Dymo.cs
@@ -17,59 +17,119 @@ namespace NodeDymoLib
             return printers;
         }
 
+        /*
+		 *
+		 *
+		 *
+		 *
+		 */
         public async Task<object> Print(object args)
         {
             IDictionary<string, object> parameters = (IDictionary<string, object>)args;
-            ILabel label = Label.Open((string)parameters["label"]);
 
-            if (null != parameters["fields"])
+            //
+            // Make sure the required parts exist
+            if (!parameters.ContainsKey("printer"))
             {
-                IDictionary<string, object> fields = (IDictionary<string, object>)parameters["fields"];
-                foreach (var kv in fields)
-                {
-                    try
-                    {
-                        var k = kv.Key.ToUpper();
-                        var obj = label.GetObjectByName(k);
-                        if (obj != null)
-                        {
-                            var v = kv.Value.ToString();
-                            label.SetObjectText(k, v);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                    }
-
-                }
+                Console.WriteLine("Dymo.cs No `printer` parameter");
+                throw new System.ArgumentException("'printer' parameter must be defined", "original");
+            } else if (!parameters.ContainsKey("labels"))
+            {
+                Console.WriteLine("Dymo.cs No `labels` parameter");
+                throw new System.ArgumentException("'labels' parameter must be defined", "original");
             }
 
-            if (null != parameters["images"])
+            //
+            // Setup the printer and this job
+            //ILabelWriterPrinter printer = Framework.GetLabelWriterPrinters()[(String)parameters["printer"]] as ILabelWriterPrinter;
+            IPrinter printer = Framework.GetPrinters()[(String)parameters["printer"]] as IPrinter;
+
+            //
+            // Set some settings for this printing
+            ILabelWriterPrintParams printParams = new LabelWriterPrintParams();
+            printParams.PrintQuality = LabelWriterPrintQuality.BarcodeAndGraphics;
+            printParams.JobTitle = "Dymo Labels";
+            printParams.Copies = (int)3;
+            if ( parameters.ContainsKey("jobTitle") )
             {
-                IDictionary<string, object> images = (IDictionary<string, object>)parameters["images"];
-                foreach (var kv in images)
+                Console.WriteLine("Dymo.cs Adding Print Job Title: " + (string)parameters["jobTitle"]);
+                printParams.JobTitle = (string)parameters["jobTitle"];
+            }
+            if( parameters.ContainsKey("copies") )
+            {
+                Console.WriteLine("Dymo.cs Adding Print Copies: " + (string)parameters["copies"]);
+                printParams.Copies = (int)parameters["copies"];
+            }
+            // Set some settings for this printing
+            //
+
+            IPrintJob printJob = printer.CreatePrintJob( printParams );
+
+            //
+            // Lets loop over these labels
+            IDictionary<string, ILabel> label = new Dictionary<string, ILabel>();
+            object[] suppliedLabels = (object[])parameters["labels"];
+            foreach (IDictionary<string, object> thisLabel in suppliedLabels)
+            {
+                var i = label.Count.ToString();
+                //var i = lkv.Key.ToUpper();
+                //IDictionary<string, object> thisLabel = (IDictionary<string, object>) lkv.Value;
+
+                //IDictionary<string, object> thisLabel = (IDictionary<string, object>)suppliedLabels[i];
+                if(  !thisLabel.ContainsKey("filename"))
                 {
-                    try
+                    Console.WriteLine("Dymo.cs No `labels`[x].`filename` parameter");
+                    throw new System.ArgumentException("'labels'.'filename' parameter must be defined for each label", "original");
+                }
+
+                label[i] = Label.Open( (string)thisLabel["filename"] );
+                if(thisLabel.ContainsKey("fields") )
+                {
+                    IDictionary<string, object> fields = (IDictionary<string, object>)thisLabel["fields"];
+                    foreach (var kv in fields)
                     {
-                        var k = kv.Key.ToUpper();
-                        var obj = label.GetObjectByName(k);
-                        if (obj != null)
+                        try
                         {
-                            var v = kv.Value;
-                            label.SetImagePngData(k, new MemoryStream((byte[])v));
+                            var k = kv.Key.ToUpper();
+                            var obj = label[i].GetObjectByName(k);
+                            if (obj != null)
+                            {
+                                var v = kv.Value.ToString();
+                                label[i].SetObjectText(k, v);
+                            }
                         }
-                    }
-                    catch (Exception ex)
-                    {
+                        catch (Exception ex) { }
+
                     }
                 }
+
+                if (null != thisLabel["images"])
+                {
+                    IDictionary<string, object> images = (IDictionary<string, object>)thisLabel["images"];
+                    foreach (var kv in images)
+                    {
+                        try
+                        {
+                            var k = kv.Key.ToUpper();
+                            var obj = label[i].GetObjectByName(k);
+                            if (obj != null)
+                            {
+                                var v = kv.Value;
+                                label[i].SetImagePngData(k, new MemoryStream((byte[])v));
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                        }
+                    }
+                }
+
+                printJob.AddLabel(label[i]);
             }
 
-            label.Print((string)parameters["printer"]);
-
-
+            
+            await Task.Run(() => printJob.Print() );
             return args;
         }
-
     }
 }
